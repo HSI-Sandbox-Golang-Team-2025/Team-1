26caab111adf6bc27d60f44ea244918e1bc82359
Ragil Burhanudin Pamungkas <sipamungkas.dev@gmail.com>
2025-09-14 14:21:17 +0700

    feat: add minimal fiber setup

--- cmd/app/main.go
+++ cmd/app/main.go
@@ -1,9 +1,25 @@
-package app
+package main
 
 import (
-	"fmt"
+	"log"
+
+	"github.com/gofiber/fiber/v2"
+	"github.com/joho/godotenv"
+	"github.com/orgs/HSI-Sandbox-Golang-Team-2025/pkg/config"
 )
 
 func main() {
-	fmt.Println("Hello, World!")
+	cfg := config.LoadEnv()
+	if err := godotenv.Load(); err != nil {
+		panic("Error loading .env file")
+	}
+
+	app := fiber.New()
+	log.Printf("Starting server in %s mode on port %s", cfg.AppEnv, cfg.AppPort)
+
+	app.Get("/", func(c *fiber.Ctx) error {
+		return c.SendString(cfg.AppEnv)
+	})
+
+	app.Listen(":" + cfg.AppPort)
 }
